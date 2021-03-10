0a34be4700f76870262f7cfcc6f59c0b80c22845
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-03-10 13:40:28 +0200

    add a new route to display lead details

--- controllers/landing.js
+++ controllers/landing.js
@@ -18,3 +18,11 @@ exports.show_leads = function(req, res, next) {
         res.render('landing', { title: 'Express', leads })
     })
 }
+
+exports.show_lead = function(req, res, next) {
+    models.Lead.findOne({
+        where: {id: req.params.id}
+    }).then(lead => {
+        res.render('lead', { lead })
+    })
+}
